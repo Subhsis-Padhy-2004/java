bc271041cb47add4835902b9f3f011343bd653f2
subhasis padhy <131591980+Subhsis-Padhy-2004@users.noreply.github.com>
2025-02-10 17:03:13 +0530

    push class_object.java

--- class_object.java
+++ class_object.java
@@ -1,3 +1,15 @@
 public class class_object {
+    int a=10;
+    String b="subhasis";
+
+    void Show(){
+        System.out.println(a+" "+b);
+    }
+}
+class Test{
+    public static void main(String[] args) {
+        class_object r=new class_object();
+        r.Show();
+    }
     
 }
