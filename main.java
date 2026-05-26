e564da7f6c32402804510289e956c3855a26c62d
Subhsis-Padhy-2004 <subhsispadhy@gmail.com>
2026-05-26 17:57:23 +0530

    output print

--- main.java
+++ main.java
@@ -1,5 +1,9 @@
 public class main{
     public static void main(String[] args){
-        System.out.print("Hello world");
+        // we use println for print next line 
+        System.out.println("Hello world");
+        //this time we can not use println but use "\n" work as same.
+        System.out.print("Hello world\n");
+        System.out.print("Hello world with java\nfrom\nsubhasis");
     }
 }
