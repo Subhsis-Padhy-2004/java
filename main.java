0ee164862f1f62c13245322ab307bac0f4c42603
Subhsis-Padhy-2004 <subhsispadhy@gmail.com>
2026-05-26 18:25:07 +0530

    Removed unwanted files

--- main.java
+++ main.java
@@ -4,6 +4,7 @@ public class main{
         System.out.println("Hello world");
         //this time we can not use println but use "\n" work as same.
         System.out.print("Hello world\n");
+        // we use multiple \n in the "duoble court" output to print next line.
         System.out.print("Hello world with java\nfrom\nsubhasis");
     }
 }
